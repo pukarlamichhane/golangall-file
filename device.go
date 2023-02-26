47df6301e78441dc2359945261503f3010f7550f
pukar <pukarlamichhane567@gmail.com>
2023-02-26 08:11:04 +0545

    first commit

---
+++ device.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 1
+	b := 2
+	c := a + b
+	d := b - a
+	e := a * b
+	f := b / a
+
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+}
